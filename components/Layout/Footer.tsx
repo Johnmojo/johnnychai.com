314c9c485414d3046e3174d993438e89dcab49f5
Johnny Chai <hello@johnnychai.com>
2022-08-31 15:51:00 +0800

    fix: footer revamp and country tag add for skillCard

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,67 +1,126 @@
-import Image from "next/image";
-
 const Footer = () => {
   return (
     <footer className="border-t pt-28 pb-20">
       <div className="mx-auto max-w-screen-xl px-8">
-        <section className="my-12">
-          <h1 className="text-3xl leading-snug md:text-5xl md:leading-snug">
-            Say hi at <br />
+        <section className="mt-12 mb-6">
+          <h1 className="pb-12 text-3xl font-medium leading-snug md:text-5xl md:leading-snug">
+            Say hi at
+          </h1>
+          <h1 className="text-xl leading-snug md:text-3xl md:leading-snug">
             <a
-              href="mailto:hello@johnnychai.com"
               target="_blank"
               rel="noreferrer"
-              className="break-words text-black underline hover:text-blue-700"
+              href="mailto:hello@johnnychai.com"
+              className="group break-words font-medium"
             >
-              hello@johnnychai.com
+              <div className="flex items-center">
+                <div className="group-hover:underline">
+                  hello@johnnychai.com
+                </div>
+                <div className="mt-auto mb-auto overflow-hidden transition-all duration-300 ease-out group-hover:-translate-y-1 group-hover:translate-x-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-8 w-8"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1"
+                    data-darkreader-inline-stroke=""
+                  >
+                    <line x1="7" y1="17" x2="17" y2="7"></line>
+                    <polyline points="7 7 17 7 17 17"></polyline>
+                  </svg>
+                </div>
+              </div>
             </a>
-            <br />
-            or stay in touch
-            <br />
           </h1>
         </section>
-        <section className="mb-36 mt-12 flex text-lg text-black">
-          <ul className="flex">
-            <li className="mr-6">
+        <section className="mb-36 mt-6 flex text-lg text-black">
+          <ul className="flex flex-col md:flex-row">
+            <li className="my-4 mr-6">
               <a
                 target="_blank"
                 rel="noreferrer"
                 href="https://www.linkedin.com/in/johchai"
+                className="group"
               >
-                <Image
-                  alt="Logo"
-                  src="/../../../static/icons/linkedin.svg"
-                  width={25}
-                  height={25}
-                />
+                <div className="flex items-center justify-between">
+                  <div>LinkedIn</div>
+                  <div className="mt-auto mb-auto overflow-hidden transition-all duration-300 ease-out group-hover:-translate-y-1 group-hover:translate-x-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                      data-darkreader-inline-stroke=""
+                    >
+                      <line x1="7" y1="17" x2="17" y2="7"></line>
+                      <polyline points="7 7 17 7 17 17"></polyline>
+                    </svg>
+                  </div>
+                </div>
               </a>
             </li>
-            <li className="mr-6">
+            <li className="my-4 mr-6">
               <a
                 target="_blank"
                 rel="noreferrer"
                 href="https://www.github.com/johnmojo"
+                className="group"
               >
-                <Image
-                  alt="Logo"
-                  src="/../../../static/icons/github.svg"
-                  width={25}
-                  height={25}
-                />
+                <div className="flex items-center justify-between">
+                  <div>Github</div>
+                  <div className="mt-auto mb-auto overflow-hidden transition-all duration-300 ease-out group-hover:-translate-y-1 group-hover:translate-x-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                      data-darkreader-inline-stroke=""
+                    >
+                      <line x1="7" y1="17" x2="17" y2="7"></line>
+                      <polyline points="7 7 17 7 17 17"></polyline>
+                    </svg>
+                  </div>
+                </div>
               </a>
             </li>
-            <li className="mr-6">
+            <li className="my-4 mr-6">
               <a
                 target="_blank"
                 rel="noreferrer"
                 href="https://www.instagram.com/johnsaaz"
+                className="group"
               >
-                <Image
-                  alt="Logo"
-                  src="/../../../static/icons/instagram.svg"
-                  width={25}
-                  height={25}
-                />
+                <div className="flex items-center justify-between">
+                  <div>Instagram</div>
+                  <div className="mt-auto mb-auto overflow-hidden transition-all duration-300 ease-out group-hover:-translate-y-1 group-hover:translate-x-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                      data-darkreader-inline-stroke=""
+                    >
+                      <line x1="7" y1="17" x2="17" y2="7"></line>
+                      <polyline points="7 7 17 7 17 17"></polyline>
+                    </svg>
+                  </div>
+                </div>
               </a>
             </li>
           </ul>
