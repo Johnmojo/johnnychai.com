eb16996998c7790f9817e7b767d803a25dc73ab0
Johnny Chai <hello@johnnychai.com>
2022-09-02 01:45:45 +0800

    fix: filename

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
 import { Logo, Arrow } from "../Content/SVG";
+import { useState, useEffect } from "react";
 
 interface Props {
   scroll: boolean;
