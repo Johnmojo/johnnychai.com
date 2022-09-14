472fd71ff40eae1cf49bbfaf478c6ac0ebe8fe21
Johnmojo <hello@johnnychai.com>
2022-09-14 13:05:09 +0800

    feat:add postcss-purgecss

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,35 @@
  */
 
 import { GetStaticProps } from "next";
+import { WorksData, BlogsData } from "../lib/types";
 import getWorks from "../lib/getWorks";
 import getBlogs from "../lib/getBlogs";
+import { Hero } from "../components/Layout/Template/Home";
+import { Summary } from "../components/Common";
+import { WorkPreview } from "../components/Layout/Template/Work";
+import { BlogPreview } from "../components/Layout/Template/Blog";
 import { HeaderMeta } from "../components/Layout/System/Header";
 
-const Home = () => {
+type Props = {
+  worksData: WorksData[];
+  blogsData: BlogsData[];
+};
+
+const Home = ({ worksData, blogsData }: Props) => {
   return (
     <>
       <HeaderMeta
         title="Home - Johnny Chai"
         description="Front End Web Developer based in Malaysia who is who is passionate about coding and solving problems through code and design."
       />
+      <Hero title="I'm Johnny, a front end developer based in Malaysia." />
+      <Summary />
+      <WorkPreview
+        data={worksData}
+        title="Some of the recent projects that I've worked on include designs, websites, and many more."
+        limit={4}
+      />
+      <BlogPreview data={blogsData} />
     </>
   );
 };
