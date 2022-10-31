ef921ec09a0267f0748bb680796eb743dd12fedb
Johnmojo <hello@johnnychai.com>
2022-10-31 11:37:17 +0800

    fix: update background color

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -26,12 +26,12 @@ const Layout = ({ children }: Props) => {
 
   // Here we store our beautiful & colorful array
   const colorsArray = [
-    "#d1e4de",
-    "#d1dee4",
-    "#d2d0e5",
-    "#e6d1d4",
-    "#e5dbd2",
-    "#eeeade"
+    "#eaeb4d", // Yellow
+    "#e2482d", // Red
+    "#007563", // Cyan
+    "#e6e6e6", // Gray
+    "#cf92ae", // Purple
+    "#ffffff" // White
   ];
 
   // Set scroll variables
